d2f1960c0a9122442923c00626df22799d5bede8
ex-tyjanii <codencart@gmail.com>
2022-06-06 17:59:53 +0100

    Updated db

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import express from 'express';
-
+import { config } from 'dotenv';
+import db from './src/utils/db.js';
 const app = express();
+config();
 app.use(express.json());
 app.listen(3000, () => {
-	console.log('js');
+	db()
+		.then(() => console.log('database Connected'))
+		.catch((err) => console.log(err));
 });
