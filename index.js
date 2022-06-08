3c0510c7308e29f1af610d09e55aa7292cf3f922
ex-tyjanii <codencart@gmail.com>
2022-06-08 15:24:29 +0100

    Login and SignUp routes completed

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 import express from 'express';
 import { config } from 'dotenv';
+import { rateLimit } from 'express-rate-limit';
+import helmet from 'helmet';
 import db from './src/utils/db.js';
+import router from './src/routes/authRouter.js';
+
 const app = express();
+app.use(helmet());
+
 config();
+
+// Limit request from same IP
+const limit = rateLimit({
+	max: 200,
+	windowMs: 60 * 60 * 1000,
+	message: 'Too many requests, please try again in an hour',
+});
+
+app.use('/api', limit);
 app.use(express.json());
-app.listen(3000, () => {
+
+app.use('/api/v1', router);
+app.listen(8080, () => {
 	db()
 		.then(() => console.log('database Connected'))
 		.catch((err) => console.log(err));
